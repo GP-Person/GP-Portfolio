bc31e5e9463b620c7a00f38e85fd17e7f2b5f304
Garrett Pearson <garrettapearson@gmail.com>
2024-11-04 14:17:55 -0800

    added logo and changed CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-//import React from 'react';
+// import React from 'react';
 import AboutMe from './pages/AboutMe';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
@@ -7,11 +7,13 @@ import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import NavTabs from '../components/NavTabs';
 import { Route, Routes } from 'react-router-dom';
+import Logo from './assets/logo.jpg'; 
 
 function App() {
   return (
     <div>
-      <header>
+      <header style={headerStyle}>
+        <img src={Logo} alt="Logo" style={logoStyle} /> 
         <h1>Garrett Pearson</h1>
         {/* NavTabs handles navigation */}
         <NavTabs />
@@ -29,18 +31,41 @@ function App() {
 
       {/* Footer */}
       <footer style={footerStyle}>
-        <div> <a href="https://www.linkedin.com/in/garrett-pearson-909854225/">LinkedIn</a> | (719) 480-3206 | <a href="mailto:garrettapearson@gmail.com" style={{ color: '#FF9900', textDecoration: 'none' }}>garrettapearson@gmail.com</a></div>
+        <div>
+          <a href="https://www.linkedin.com/in/garrett-pearson-909854225/" style={linkStyle}>LinkedIn</a> | (719) 480-3206 | 
+          <a href="mailto:garrettapearson@gmail.com" style={{ color: '#FF9900', textDecoration: 'none' }}> garrettapearson@gmail.com</a>
+        </div>
       </footer>
     </div>
   );
 }
 
-// Inline styling for the footer
+// Inline styling
+const headerStyle = {
+  backgroundColor: '#333',
+  color: '#FF9900',
+  padding: '1rem',
+  textAlign: 'center' as const,
+};
+
 const footerStyle = {
   padding: '1rem',
   backgroundColor: '#333',
   color: '#FFF',
   marginTop: '2rem',
+};
+
+const logoStyle = {
+  height: '60px',
+  width: '60px', 
+  borderRadius: '50%', 
+  marginBottom: '0.5rem',
+  objectFit: 'cover', 
+};
+
+const linkStyle = {
+  color: '#FF9900',
+  textDecoration: 'none',
 };
 
 export default App;
